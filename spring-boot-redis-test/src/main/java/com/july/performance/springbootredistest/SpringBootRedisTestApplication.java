66bdb2ced3699b36d37c8653b6c301ba79cb9dde
Coder.July <god@me.com>
2021-04-25 16:50:01 +0800

    log4j2

--- spring-boot-redis-test/src/main/java/com/july/performance/springbootredistest/SpringBootRedisTestApplication.java
+++ spring-boot-redis-test/src/main/java/com/july/performance/springbootredistest/SpringBootRedisTestApplication.java
@@ -1,13 +1,16 @@
 package com.july.performance.springbootredistest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootRedisTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRedisTestApplication.class, args);
+        log.info("测试启动日志");
     }
 
 }
--- spring-boot-redis-test/src/test/java/com/july/performance/springbootredistest/SpringBootRedisTestApplicationTests.java
+++ spring-boot-redis-test/src/test/java/com/july/performance/springbootredistest/SpringBootRedisTestApplicationTests.java
@@ -1,5 +1,7 @@
 package com.july.performance.springbootredistest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,17 @@ class SpringBootRedisTestApplicationTests {
     @Test
     void contextLoads() {
     }
+
+    @Test
+    public void testLog4j2(){
+        Logger logger = LogManager.getLogger(SpringBootRedisTestApplicationTests.class);
+        logger.error("...");
+        logger.warn("...");
+        logger.info("...");
+        logger.debug("...");
+        logger.trace("...");
+    }
+
+
 
 }
