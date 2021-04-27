8eb3f9986df10f91276b03a40c2f42039565a19c
Coder.July <god@me.com>
2021-04-27 13:49:19 +0800

    add:log4j2

--- spring-boot-redis-test/src/main/java/com/july/performance/springbootredistest/SpringBootRedisTestApplication.java
+++ spring-boot-redis-test/src/main/java/com/july/performance/springbootredistest/SpringBootRedisTestApplication.java
@@ -1,16 +1,23 @@
 package com.july.performance.springbootredistest;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.core.async.AsyncLoggerContextSelector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.Assert;
 
+/** @author july */
 @SpringBootApplication
 @Slf4j
 public class SpringBootRedisTestApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootRedisTestApplication.class, args);
-        log.info("测试启动日志");
-    }
+  static {
+    Assert.isTrue(AsyncLoggerContextSelector.isSelected(), "log4j2 的异步 disruptor启动失败");
+    log.info("AsyncLoggerContextSelector.isSelected():{}",AsyncLoggerContextSelector.isSelected());
+  }
 
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootRedisTestApplication.class, args);
+    log.info("测试启动日志");
+  }
 }
